65d2a341bfc39bfb158ebd780c8f0771a08535db
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-09-30 21:24:38 -0400

    remove __repr__/origin mentions and update README

--- classes/plant.py
+++ classes/plant.py
@@ -1,8 +1,7 @@
 class Plant:
-    def __init__(self, name, water_level, origin, sunlight_hours, is_blooming=False):
+    def __init__(self, name, water_level, sunlight_hours, is_blooming=False):
         self.name = name
         self.water_level = water_level
-        self.origin = origin
         self.sunlight_hours = sunlight_hours
         self.is_blooming = is_blooming
     
@@ -15,9 +14,9 @@ class Plant:
         growth_stage = None
 
         if self.water_level > 5 and self.sunlight_hours > 5:
-            growth_stage = "Sprout"
-        if self.water_level > 10 and self.sunlight_hours > 10:
             growth_stage = "Mature"
+        else:
+            growth_stage = "Sprout"
         
         return f"{self.name} is currently at the {growth_stage} stage."
     
@@ -25,8 +24,3 @@ class Plant:
         self.sunlight_hours += hours
 
         return f"{self.name} has received a total of {hours}. New sunlight level: {self.sunlight_hours}"
-    
-    def __repr__(self):
-        return f"Plant(name='{self.name}', origin='{self.origin}', water_level={self.water_level}, sunlight_hours={self.sunlight_hours}, is_blooming={self.is_blooming})"
-
-    
